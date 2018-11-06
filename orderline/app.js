a46477ba9d9455df3ca1089646b9578d9d120549
Mert Çeken <mertceken12@gmail.com>
2018-11-06 12:05:12 +0300

    Order Line with Angular.Js

---
+++ orderline/app.js
@@ -0,0 +1,44 @@
+function OrderFormController($scope){
+
+    // Model özellikleri tanımlanması
+    $scope.services = [
+        {
+            name: 'Web Development',
+            price: 300,
+            active:true
+        },{
+            name: 'Design',
+            price: 400,
+            active:false
+        },{
+            name: 'Integration',
+            price: 250,
+            active:false
+        },{
+            name: 'Training',
+            price: 220,
+            active:false
+        }
+    ];
+
+    $scope.toggleActive = function(s){
+        s.active = !s.active;
+    };
+
+    // Toplam fiyatı hesaplamaya yardımcı olan metod
+
+    $scope.total = function(){
+
+        var total = 0;
+
+      
+
+        angular.forEach($scope.services, function(s){
+            if (s.active){
+                total+= s.price;
+            }
+        });
+
+        return total;
+    };
+}
