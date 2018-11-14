ca92e05250dd96501b26d5be2965c62481cbf000
Mert Çeken <mertceken12@gmail.com>
2018-11-14 12:36:41 +0300

    Draggable menu
    Draggable menu practice with vue.js

---
+++ vue-draggable/app.js
@@ -0,0 +1,196 @@
+var STORAGE_KEY = 'todos-vuejs-2.0'
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach(function (todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+// visibility filters
+var filters = {
+  all: function (todos) {
+    return todos
+  },
+  active: function (todos) {
+    return todos.filter(function (todo) {
+      return !todo.completed
+    })
+  },
+  completed: function (todos) {
+    return todos.filter(function (todo) {
+      return todo.completed
+    })
+  }
+}
+
+// app Vue instance
+var app = new Vue({
+  // app initial state
+  data: {
+    todos: todoStorage.fetch(),
+    newTodo: '',
+    editedTodo: null,
+    visibility: 'all'
+  },
+
+  // watch todos change for localStorage persistence
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
+  },
+
+  // computed properties
+  // http://vuejs.org/guide/computed.html
+  computed: {
+    filteredTodos: function () {
+      return filters[this.visibility](this.todos)
+    },
+    remaining: function () {
+      return filters.active(this.todos).length
+    },
+    allDone: {
+      get: function () {
+        return this.remaining === 0
+      },
+      set: function (value) {
+        this.todos.forEach(function (todo) {
+          todo.completed = value
+        })
+      }
+    }
+  },
+
+  filters: {
+    pluralize: function (n) {
+      return n === 1 ? 'item' : 'items'
+    }
+  },
+
+  // methods that implement data logic.
+  // note there's no DOM manipulation here at all.
+  methods: {
+    addTodo: function () {
+      var value = this.newTodo && this.newTodo.trim()
+      if (!value) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        title: value,
+        completed: false
+      })
+      this.newTodo = ''
+    },
+
+    removeTodo: function (todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    },
+
+    editTodo: function (todo) {
+      this.beforeEditCache = todo.title
+      this.editedTodo = todo
+    },
+
+    doneEdit: function (todo) {
+      if (!this.editedTodo) {
+        return
+      }
+      this.editedTodo = null
+      todo.title = todo.title.trim()
+      if (!todo.title) {
+        this.removeTodo(todo)
+      }
+    },
+
+    cancelEdit: function (todo) {
+      this.editedTodo = null
+      todo.title = this.beforeEditCache
+    },
+
+    removeCompleted: function () {
+      this.todos = filters.active(this.todos)
+    }
+  },
+
+  // a custom directive to wait for the DOM to be updated
+  // before focusing on the input field.
+  // http://vuejs.org/guide/custom-directive.html
+  directives: {
+    'todo-focus': function (el, binding) {
+      if (binding.value) {
+        el.focus()
+      }
+    }
+  }
+})
+
+Vue.component('draggable-header-view', {
+  template: '#header-view-template',
+  data: function () {
+    return {
+      dragging: false,
+      // quadratic bezier control point
+      c: { x: 160, y: 160 },
+      // record drag start point
+      start: { x: 0, y: 0 }
+    }
+  },
+  computed: {
+    headerPath: function () {
+      return 'M0,0 L320,0 320,160' +
+        'Q' + this.c.x + ',' + this.c.y +
+        ' 0,160'
+    },
+    contentPosition: function () {
+      var dy = this.c.y - 160
+      var dampen = dy > 0 ? 2 : 4
+      return {
+        transform: 'translate3d(0,' + dy / dampen + 'px,0)'
+      }
+    }
+  },
+  methods: {
+    startDrag: function (e) {
+      e = e.changedTouches ? e.changedTouches[0] : e
+      this.dragging = true
+      this.start.x = e.pageX
+      this.start.y = e.pageY
+    },
+    onDrag: function (e) {
+      e = e.changedTouches ? e.changedTouches[0] : e
+      if (this.dragging) {
+        this.c.x = 160 + (e.pageX - this.start.x)
+        // dampen vertical drag by a factor
+        var dy = e.pageY - this.start.y
+        var dampen = dy > 0 ? 1.5 : 4
+        this.c.y = 160 + dy / dampen
+      }
+    },
+    stopDrag: function () {
+      if (this.dragging) {
+        this.dragging = false
+        dynamics.animate(this.c, {
+          x: 160,
+          y: 160
+        }, {
+          type: dynamics.spring,
+          duration: 700,
+          friction: 280
+        })
+      }
+    }
+  }
+})
+
+new Vue({ el: '#app' })
