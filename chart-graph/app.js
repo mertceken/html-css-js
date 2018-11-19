5fb024144bd026fa56e68414e8bb35e8500291e8
Mert Çeken <mertceken12@gmail.com>
2018-11-19 16:20:13 +0300

    Chart.Js Intro

---
+++ chart-graph/app.js
@@ -0,0 +1,16 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    datasets: [{
+      label: 'apples',
+      data: [12, 19, 3, 17, 28, 24, 7],
+      backgroundColor: "rgba(153,255,51,1)"
+    }, {
+      label: 'oranges',
+      data: [30, 29, 5, 5, 20, 3, 10],
+      backgroundColor: "rgba(255,153,0,1)"
+    }]
+  }
+});
